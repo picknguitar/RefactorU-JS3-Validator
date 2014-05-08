ea099369645e8d456c21673eca0129443b79462a
Nick Walker <nwalker75@yahoo.com>
2014-05-08 11:40:23 -0600

    first commit. validation for user info in forms/input areas.

---
+++ main.js
@@ -0,0 +1,16 @@
+var phoneNumber = prompt("Please enter your phone number, (xxx-xxx-xxxx).");
+
+
+	 
+
+
+var birthDate = prompt("Please enter your birthday, (month/day/year (xx/xx/xx))."); 
+
+
+// var postalCode = prompt("Please enter your postal code, (xxxxx OR xxxxx-xxxx).");
+
+
+// var stateAbbr = prompt("Please enter your state, i.e., CO");
+
+
+// var married = prompt("Are you married?");
